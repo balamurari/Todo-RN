ae3a01c56de1de5c68a2f3bbccc148df85d1b000
Bala Murari Gadde <balamurari2003@gmail.com>
2025-03-31 00:33:49 +0530

    Splash Screen Added

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Image, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Logo from '../assets/images/icon.png'
+import { screenHeight, screenWidth } from '../utils/Constants'
+import { useFonts } from 'expo-font'
+import { resetAndNavigate } from '../utils/Helpers'
 
-const index = () => {
+const Main = () => {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+
+  const[hasNavigated, setHasNavigated] = useState(false)
+
+  useEffect(()=>{
+    if (loaded && !hasNavigated){
+      const timeoutId = setTimeout(()=>{
+        resetAndNavigate('/home')
+      },1000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  },[loaded, hasNavigated])
+
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text>Todo Application</Text>
     </View>
   )
 }
 
-export default index
+export default Main
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  img:{
+    width:screenWidth * 0.3,
+    height:screenHeight * 0.12,
+  }
+})
---
+++ src/utils/Constants.js
@@ -0,0 +1,6 @@
+import { Dimensions } from "react-native"
+
+export const screenWidth = Dimensions.get('window').width
+export const screenHeight = Dimensions.get('window').height
+
+//dimensions for the screen according to the phone screen size
